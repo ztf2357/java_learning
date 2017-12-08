e793e81132a147a3af97ba540c1239f8e63262a5
ztf <ztf2357@sina.com>
2017-12-08 21:25:57 +0800

    新增了JSON对网页的解析

--- exam/src/main/java/com/beyondhost/exam/service/CrawlerService.java
+++ exam/src/main/java/com/beyondhost/exam/service/CrawlerService.java
@@ -1,20 +1,90 @@
 package com.beyondhost.exam.service;
 
+import java.io.IOException;
 import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 import com.beyondhost.exam.entity.ConfigKeys;
+import com.beyondhost.exam.entity.OrgInfo;
 import com.beyondhost.exam.util.ConfigHelper;
 import com.beyondhost.exam.util.HttpHelper;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CrawlerService  {
 
-    public String getWebPageContent() {
-        String urlFomat = ConfigHelper.getValue(ConfigKeys.ORG_INFO_URL_FORMAT);
-        String fullUrl = MessageFormat.format(urlFomat,158385112,"2017-12-07","2017-12-07");
-        return HttpHelper.GetContent(fullUrl);
+    private static final Logger logger = LoggerFactory.getLogger(CrawlerService.class);
 
+    public String getWebPageContent()
+    {
+        String urlFomat = ConfigHelper.getValue(ConfigKeys.ORG_INFO_URL_FORMAT);
+        String fullUrl = MessageFormat.format(urlFomat,158385112,"2017-12-07","2017-12-08");
+        String htmlString =  HttpHelper.GetContent(fullUrl);
+        return htmlString;
+    }
+
+    public OrgInfo parseWebPageContent() {
+        String urlFomat = ConfigHelper.getValue(ConfigKeys.ORG_INFO_URL_FORMAT);
+        String fullUrl = MessageFormat.format(urlFomat,158385112,"2017-12-07","2017-12-08");
+        String htmlString =  HttpHelper.GetContent(fullUrl);
+        Document document = Jsoup.parse(htmlString);
+        String  scriptText =  document.getElementsByTag("script").first().html();
+        String tempString =  scriptText.replace("window.__INITIAL_STATE__=","");
+        String jsonString = tempString.substring(0,tempString.length()-1);
+        ObjectMapper mapper = new ObjectMapper();
+        OrgInfo info = new OrgInfo();
+        try {
+            JsonNode node = mapper.readTree(jsonString);
+            JsonNode poiData = node.get("poiData");
+            info.setPhone(poiData.get("phone").asText());
+            info.setAddress(poiData.get("addr").asText());
+            info.setCityId(poiData.get("cityId").asInt());
+            info.setLatitude(poiData.get("lat").asDouble());
+            info.setLatitude(poiData.get("lng").asDouble());
+
+            info.setPoiId(poiData.get("poiid").asLong());
+            info.setOrgName(poiData.get("name").asText());
+
+            Iterator<JsonNode> poiExtendsInfos = node.get("poiExt").get("hotelIntroInfo").get("poiExtendsInfos").elements();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+            while(poiExtendsInfos.hasNext())
+            {
+                JsonNode extAttr = poiExtendsInfos.next();
+                int attrId = extAttr.get("attrId").asInt();
+                if(attrId==101120)
+                {
+                    String yearString = extAttr.get("attrValue").asText().replace("年","");
+                    try {
+                        Date openDate = sdf.parse(yearString + "-01-01");
+                        info.setOpenTime(openDate);
+                    }
+                    catch (java.text.ParseException ex)
+                    {
+                        logger.error("解析html格式化日期出错",ex);
+                    }
+                }
+                if(attrId==101122)
+                {
+                   String  roomCountString =  extAttr.get("attrValue").asText().replace("间","");
+                   info.setRoomCount(Integer.parseInt(roomCountString));
+                }
+            }
+
+        }catch(IOException ex)
+        {
+            logger.error("解析html json出错",ex);
+        }
+        return info;
     }
 }
--- exam/src/main/java/com/beyondhost/exam/util/HttpHelper.java
+++ exam/src/main/java/com/beyondhost/exam/util/HttpHelper.java
@@ -1,13 +1,12 @@
 package com.beyondhost.exam.util;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
+import org.apache.http.*;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +21,15 @@ public final class HttpHelper {
     {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         try {
-            HttpGet httpget = new HttpGet(url);
+            HttpGet httpGet = new HttpGet(url);
+            httpGet.setHeader(HttpHeaders.HOST,"hotel.meituan.com");
+            httpGet.setHeader(HttpHeaders.CONNECTION,"keep-alive");
+            httpGet.setHeader(HttpHeaders.ACCEPT,"*/*");
+            httpGet.setHeader(HttpHeaders.USER_AGENT,"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36");
+            httpGet.setHeader(HttpHeaders.ACCEPT_ENCODING,"gzip, deflate, br");
+            httpGet.setHeader(HttpHeaders.ACCEPT_LANGUAGE,"zh-CN,zh;q=0.9");
+            httpGet.setHeader("Cookie","IJSESSIONID=lfx31zhoz4pl1ef1ahcfs2tvy; iuuid=58041698C0D5DD2D72F152B3E335416E4AD943311069BA2FD6EA87E91F3EFD86; latlng=\"31.127014,121.360474,1512731376105\"; ci=10; cityname=%E4%B8%8A%E6%B5%B7; _lxsdk_cuid=16035d0fb6dc8-05e87db276b425-5a442916-1fa400-16035d0fb6dc8; uuid=3edfba9b0cd54d688b8e.1512731376.1.0.0; hotel_city_id=1; hotel_city_info=%7B%22id%22%3A1%2C%22name%22%3A%22%E5%8C%97%E4%BA%AC%22%2C%22pinyin%22%3A%22beijing%22%7D; __mta=141332749.1512731377911.1512737823621.1512737904579.7; _lxsdk_s=160362c58ee-de3-2d0-8b%7C%7C8");
+
             ResponseHandler<String> responseHandler = (response) -> {
                     int status = response.getStatusLine().getStatusCode();
                     if (status >= HttpStatus.SC_OK && status < HttpStatus.SC_MULTIPLE_CHOICES) {
@@ -32,7 +39,7 @@ public final class HttpHelper {
                         throw new ClientProtocolException("Unexpected response status: " + status);
                     }
                 };
-            String responseBody = httpclient.execute(httpget, responseHandler);
+            String responseBody = httpclient.execute(httpGet, responseHandler);
             return responseBody;
         }
         catch (IOException ex)
